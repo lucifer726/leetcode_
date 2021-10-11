4e3549eceefd3d14dd1d33c81c9e048eb813bb36
lucifer <18856419225@163.com>
2021-10-11 11:54:33 +0800

    给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。

---
+++ 二分查找.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums, target: int) -> int:
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            mid = (high - low) // 2 + low
+            num = nums[mid]
+            if num == target:
+                return mid
+            elif num > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
+
+
+a = Solution()
+nums = [-1, 0, 3, 5, 9, 12]
+target = 9
+print(a.search(nums, target))
