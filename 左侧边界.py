760500f6fb17f52bad1c571bff7e8cbf724655fd
lucifer <18856419225@163.com>
2022-01-12 23:05:26 +0800

    add 左侧边界

--- 左侧边界.py
+++ 左侧边界.py
@@ -1,19 +1,22 @@
 def left_bound(nums, target):
     if len(nums) == 0: return -1
     left = 0
-    right = len(nums)
+    right = len(nums) - 1
 
-    while left < right:
+    while left <= right:
         mid = left + (right - left) // 2
         if nums[mid] == target:
-            right = mid
+            right = mid - 1
         elif nums[mid] > target:
-            right = mid
+            right = mid - 1
         elif nums[mid] < target:
             left = mid + 1
+
+    if left > len(nums) - 1 or nums[left] != target:
+        return -1
     return left
 
 
-nums = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4]
+nums = [1, 1, 2, 2, 3, 3, 4, 4]
 target = 2
 print(left_bound(nums, target))
