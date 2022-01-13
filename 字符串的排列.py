341ef082790e6fb504ef92083470c4c34f682b34
lucifer <18856419225@163.com>
2022-01-13 22:58:17 +0800

    add 字符串的排列.py

--- 字符串的排列.py
+++ 字符串的排列.py
@@ -1,16 +1,20 @@
+from collections import defaultdict
+
+
 def checkInclusion(s1, s2):
-    need, window = {}, {}
-    for c in s1:
-        need[c] = need.setdefault(c, 0) + 1
+    need, window = defaultdict(int), defaultdict(int)
+    for c in s1: need[c] += 1
+
     left, right = 0, 0
     valid = 0
     while right < len(s2):
         c = s2[right]
         right += 1
         if c in need:
-            window[c] = window.setdefault(c, 0) + 1
+            window[c] += 1
             if window[c] == need[c]:
                 valid += 1
+
         while right - left >= len(s1):
             if valid == len(need):
                 return True
@@ -20,12 +24,10 @@ def checkInclusion(s1, s2):
                 if window[d] == need[d]:
                     valid -= 1
                 window[d] -= 1
+
     return False
+
 
 s1 = "ab"
 s2 = "eidbaooo"
-print(checkInclusion(s1,s2))
-
-
-
-
+print(checkInclusion(s1, s2))
