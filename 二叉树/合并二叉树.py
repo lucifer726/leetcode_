cf301195657c6b977d1fe1e6e9bdaca977640154
sunhao <18856419225@163.com>
2022-06-17 17:36:34 +0800

    20220617 add

---
+++ 二叉树/合并二叉树.py
@@ -0,0 +1,14 @@
+# 已知两颗二叉树，将它们合并成一颗二叉树。合并规则是：都存在的结点，就将结点值加起来，否则空的位置就由另一个树的结点来代替。
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        # write code here
+        if not t1 and not t2:
+            return None
+        if not t1:
+            return t2
+        if not t2:
+            return t1
+        t1.val += t2.val
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
+        return t1
