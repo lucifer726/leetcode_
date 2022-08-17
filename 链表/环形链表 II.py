5324fa03eb0a45b18b18da1ade5d349077577ec5
sunhao <18856419225@163.com>
2022-08-17 22:07:31 +0800

    20220817 add

---
+++ 链表/环形链表 II.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            # 如果相遇
+            if slow == fast:
+                p = head
+                q = slow
+                while p != q:
+                    p = p.next
+                    q = q.next
+                # 也可以return q
+                return p
+
+        return None
