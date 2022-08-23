8b5aff513ad565f5b7e74f2595745e96ac1e704b
sunhao <18856419225@163.com>
2022-08-23 23:15:44 +0800

    20220823 add

---
+++ 哈希表/四数相加 II.py
@@ -0,0 +1,19 @@
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        # use a dict to store the elements in nums1 and nums2 and their sum
+        hashmap = dict()
+        for n1 in nums1:
+            for n2 in nums2:
+                if n1 + n2 in hashmap:
+                    hashmap[n1 + n2] += 1
+                else:
+                    hashmap[n1 + n2] = 1
+
+        # if the -(a+b) exists in nums3 and nums4, we shall add the count
+        count = 0
+        for n3 in nums3:
+            for n4 in nums4:
+                key = - n3 - n4
+                if key in hashmap:
+                    count += hashmap[key]
+        return count
